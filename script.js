5b877a1c7105ce2b7652b7c80eb35e9ae9075991
taep823 <apetty3@cps.edu>
2025-03-17 15:08:22 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("hello")
+let height = document.querySelector(".height")
+console.log("height");
+let power = document.querySelector(".power")
+console.log("power");
+let quiz= document.querySelector(".quiz")
+console.log("quiz");
+
+quiz.onclick = function(){
+console.log("clicked")
+height = height.value;
+console.log("height")
+power = power.value;
+console.log("power")
+if (height === "tall" && power === "Flight"){
+}
+console.log("good")
+};
