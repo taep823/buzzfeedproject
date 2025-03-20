9cb95ed0b2609a0975330b63253fda7e0fa1475d
taep823 <apetty3@cps.edu>
2025-03-20 16:26:45 +0000

    v3

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ let power = document.querySelector(".power")
 console.log("power");
 let quiz= document.querySelector(".quiz")
 console.log("quiz");
+let message= document.querySelector(".message")
 
 quiz.onclick = function(){
+
+let ans1 = height.value;
+let ans2 = power.value;
+
 console.log("clicked")
-height = height.value;
-console.log("height")
-power = power.value;
-console.log("power")
-if (height === "tall" && power === "Flight"){
+if (ans1 === "Tall" && ans2 === "Flight"){
+    message.innerHTML="You are Gumball!";
+    console.log("tallfly")
+}else if(ans1 === "Tall" && ans2 === "Time Travel"){
+    message.innerHTML="You are Darwin!";
+console.log("talltime")
+}else if (ans1==="Short" && ans2==="Flight"){
+message.innerHTML="You are Darwin!";
 }
-console.log("good")
 };
