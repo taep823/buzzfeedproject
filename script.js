9fe0db2197f009ce280a944bface7e645b5678f4
taep823 <apetty3@cps.edu>
2025-04-02 16:27:13 +0000

    v5

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 console.log("hello")
 let height = document.querySelector(".height")
 console.log("height");
+
 let power = document.querySelector(".power")
 console.log("power");
+
 let quiz= document.querySelector(".quiz")
 console.log("quiz");
+
 let message= document.querySelector(".message")
-let gumimg=document.querySelector(".gumimg")
+
+let gumImg=document.querySelector(".gumImg")
+gumImg.src= "gumballimg.webp";
+
+
 quiz.onclick = function(){
 
 let ans1 = height.value;
+
 let ans2 = power.value;
 
-gumimg.style.display="none"
 console.log("clicked")
+
 if (ans1 === "Tall" && ans2 === "Flight"){
-    gumimg.style.display="block"
+    gumImg.src= "jumpingGum.webp";
     message.innerHTML="You are Gumball!";
     console.log("tallfly")
+
 }else if(ans1 === "Tall" && ans2 === "Time Travel"){
-    gumimg.style.display="none"
     message.innerHTML="You are Darwin!";
+    gumImg.src="darwin.webp" 
 console.log("talltime")
+
 }else if (ans1==="Short" && ans2==="Flight"){
 message.innerHTML="You are Darwin!";
-gumimg.style.display="none"
+gumImg.src="darwin.webp"
+
+
 }else if (ans1==="Short" && ans2==="Time Travel"){
     message.innerHTML="You are Gumball!";
-    gumimg.style.display="block"
+    gumImg.src= "jumpingGum.webp";
 }
 };
 
